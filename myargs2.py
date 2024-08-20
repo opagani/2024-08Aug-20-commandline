d6ab1e576e29949f5a464ffb0f62e648602c6b1b
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 18:40:51 +0300

    gitautosync update 37

--- myargs2.py
+++ myargs2.py
@@ -6,9 +6,9 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Define some arguments
-parser.add_argument('a')
-parser.add_argument('b')
-parser.add_argument('c')
+parser.add_argument('-a')
+parser.add_argument('-b')
+parser.add_argument('-c')
 
 # Parse the user's arguments (in sys.args) according to the rules/args that we defined
 args = parser.parse_args()
