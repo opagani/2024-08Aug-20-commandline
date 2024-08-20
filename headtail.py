be03cbccd9bbedf86929c61514ca09446cc0df3e
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 19:22:05 +0300

    gitautosync update 72

---
+++ headtail.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+
+    # Unix has two utilities, head and tail, which show us a number of lines at the start of a file, or the end of a file, respectively.
+    # Write a program, headtail, that takes three arguments:
+    #     --head, whose value is an integer indicating how many lines from the start of the file should be shown, with a default of 0
+    #     --tail, whose value is an integer indicating how many lines from the end of the file should be shown, with a default of 0
+    #     --file, the filename from which we'll read
