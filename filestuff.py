2a3e3e458ce98bd2c628d5766d956775f1566c97
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 20:51:19 +0300

    gitautosync update 143

--- filestuff.py
+++ filestuff.py
@@ -5,7 +5,10 @@ import os
 
 class Filestuff(cmd.Cmd):
     def do_length(self, line):
-        print(os.stat(line))
+        if os.path.exists(line):
+            print(os.stat(line).st_size)
+        else:
+            print(f'{line} does not exist; try again')
 
     def do_quit(self, line):
         return True
