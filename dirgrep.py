641a5878f981dd8ce9b18e4526c6c0620d768ba3
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 20:10:53 +0300

    gitautosync update 98

--- dirgrep.py
+++ dirgrep.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
+import glob
 
 # create an argument parser object
 parser = argparse.ArgumentParser()
 
 # Define some arguments
-parser.add_argument('--head', type=int, default=0)
-parser.add_argument('--tail', type=int, default=0)
-parser.add_argument('-f', '--file', type=argparse.FileType('r'))
+parser.add_argument('--dirname', type=str)
+parser.add_argument('--text', type=str)
 
 # Parse the user's arguments (in sys.args) according to the rules/args that we defined
 args = parser.parse_args()
 
 # Now I can open the file, and read "args.number" characters from it
 
-all_lines = args.file.readlines()
-
-if args.head:
-    print(''.join(all_lines[:args.head]))
-
-if args.head and args.tail:
-    print('----')
-
-if args.tail:
-    print(''.join(all_lines[-args.tail:]))
+for one_filename in glob.glob(f'{dirname}/*'):
+    print(one_filename)
