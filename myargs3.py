c0e9097f67306a161fc02ef6b5a3a60b4f5bfd34
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 18:46:01 +0300

    gitautosync update 46

--- myargs3.py
+++ myargs3.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 # Define some arguments
 parser.add_argument('-a', '--apple', type=int)
 parser.add_argument('-b', '--banana', type=int)
-parser.add_argument('-c', '--cucumber', type=str, default='(no value)')
+parser.add_argument('-c', '--cucumber', type=str, default='(no cucumbers today)')
 
 # Parse the user's arguments (in sys.args) according to the rules/args that we defined
 args = parser.parse_args()
