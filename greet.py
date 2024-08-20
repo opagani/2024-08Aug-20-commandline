7a457985d1d27c17ace8a3559024e1ef2374a223
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 18:39:14 +0300

    gitautosync update 34

--- greet.py
+++ greet.py
@@ -6,15 +6,11 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Define some arguments
-parser.add_argument('a')
-parser.add_argument('b')
-parser.add_argument('c')
+parser.add_argument('first')
+parser.add_argument('last')
 
 # Parse the user's arguments (in sys.args) according to the rules/args that we defined
 args = parser.parse_args()
-
-# print the args namespace object that we got
-print(args)
 
 # all of the arguments are now available as attributes on the "args" object
 print(f'{args.a=}')
