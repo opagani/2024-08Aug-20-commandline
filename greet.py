acbb5bea9019c7fd682cf4d1996ed61c54ae73bc
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 18:39:26 +0300

    gitautosync update 35

--- greet.py
+++ greet.py
@@ -12,7 +12,4 @@ parser.add_argument('last')
 # Parse the user's arguments (in sys.args) according to the rules/args that we defined
 args = parser.parse_args()
 
-# all of the arguments are now available as attributes on the "args" object
-print(f'{args.a=}')
-print(f'{args.b=}')
-print(f'{args.c=}')
+print(f'Hello, {args.first} {args.last}!')
