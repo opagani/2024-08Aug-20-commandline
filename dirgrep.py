5cfc60fa8d4cd4b5ec1ab01ea28b4b86ad1c488d
Reuven Lerner <reuven@lerner.co.il>
2024-08-20 20:10:01 +0300

    gitautosync update 97

---
+++ dirgrep.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+
+# create an argument parser object
+parser = argparse.ArgumentParser()
+
+# Define some arguments
+parser.add_argument('--head', type=int, default=0)
+parser.add_argument('--tail', type=int, default=0)
+parser.add_argument('-f', '--file', type=argparse.FileType('r'))
+
+# Parse the user's arguments (in sys.args) according to the rules/args that we defined
+args = parser.parse_args()
+
+# Now I can open the file, and read "args.number" characters from it
+
+all_lines = args.file.readlines()
+
+if args.head:
+    print(''.join(all_lines[:args.head]))
+
+if args.head and args.tail:
+    print('----')
+
+if args.tail:
+    print(''.join(all_lines[-args.tail:]))
